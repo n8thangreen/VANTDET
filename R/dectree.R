a51d6e1c4891d507a46b6b5a7b550bf24186befb
Dr Nathan Green <nathan.green@imperial.ac.uk>
2018-01-12 11:09:58 +0000

    main decision tree function

--- R/dectree.R
+++ R/dectree.R
@@ -8,19 +8,19 @@
 #' @param nsim Number of sample points (integer) Default: 1000
 #' @param costDistns List of distribution names and parameter values for each test/procedure
 #' @param time_res time to obtain novel test result (list)
+#' @param drug treatment drug cost and quantities (list)
 #' @param prevalence TB proportion in cohort (double 0-1) Default: 0.25
-#' @param cutoff Clinical judgement threshold (double 0-1) Default: 1
-#' @param FNtime False negative follow-up time fixed value (double) Default: 42
-#' @param FNdist Should false negative time to follow-up distribution be used (logical) Default: TRUE
 #' @param performance Sensitivity and specificity test (list)
-#' @param c.ruleout Rule-out test unit cost (double) Defalut: 0
-#' @param name.ruleout Name of rule-out test to get at distribution (string)
+#' @param c.newtest Rule-out test unit cost (double) Defalut: 0
+#' @param name.newtest Name of rule-out test to get at distribution (string)
 #' @param quant Quantile value of time to diagnosis and costs from observed data (double 0-1) Default: 0.5 (median)
-#' @param utility QALY adjustment utility due to active TB
+#' @param QALYloss QALY loss due to active TB (list)
 #' @param N Number of patients. The number in the data is used as default
 #' @param wholecohortstats Should the output stats be the total or per patient
 #' @param terminal_cost function of terminal node total costs
 #' @param terminal_health function of terminal node total QALY loss
+#' @param followup_pdf Follow-up appointment time probability distribution function
+#' @param ...
 #'
 #' @return Health and cost realisations (list)
 #'
@@ -30,32 +30,28 @@ dectree <- function(data,
                     nsim = 100,
                     costDistns,
                     time_res,
+                    drug = drug,
                     prevalence = 0.25,
-                    cutoff = 1, #ie no clinical judgement
-                    FNtime = 42,
-                    FNdist = TRUE,
                     performance,
-                    c.ruleout = 0,
-                    name.ruleout = NA,
+                    c.newtest = 0,
+                    name.newtest = NA,
                     quant = 0.5, #median
-                    utility = NA,
+                    QALYloss,
                     N = nrow(data),
                     wholecohortstats = FALSE,
                     terminal_health,
-                    terminal_cost) {
+                    terminal_cost,
+                    followup_pdf, ...) {
 
   # require(assertive)
   require(triangle)
 
-  stopifnot(name.ruleout %in% c(NA, names(costDistns)))
+  stopifnot(name.newtest %in% c(NA, names(costDistns)))
   stopifnot(quant >= 0, quant <= 1)
   stopifnot(nsim > 0,
             prevalence >= 0,
             prevalence <= 1,
-            c.ruleout >= 0,
-            cutoff >= 0,
-            cutoff <= 1,
-            FNtime >= 0)
+            c.newtest >= 0)
 
   median <- purrr::partial(...f = quantile, probs = quant, na.rm = TRUE)
 
@@ -64,91 +60,71 @@ dectree <- function(data,
   if (wholecohortstats) N <- 1
 
   # 2 month follow-up appointment times
-  FNdens <-
-    as.numeric(data$start.to.FU) %>%
-    subset(data$VisitFU == "2 month FU" &
-             data$DosanjhGrouped %in% c(1,2) &
-             !is.na(data$start.to.FU) &
-             data$start.to.FU <= 200 &
-             data$start.to.FU > 0) %>%
-    density(from = 0, bw = 10)
+  if (missing(followup_pdf)) {
+    followup_pdf  <-
+      as.numeric(data$start.to.FU) %>%
+      subset(data$VisitFU == "2 month FU" &
+               data$DosanjhGrouped %in% c(1,2) &
+               !is.na(data$start.to.FU) &
+               data$start.to.FU <= 200 &
+               data$start.to.FU > 0) %>%
+      density(from = 0, bw = 10)
+  }
 
-  Fx <- cumsum(FNdens$y)/sum(FNdens$y)
-
+  followup_cdf <- data.frame(x = followup_pdf$x,
+                             y = cumsum(followup_pdf$y)/sum(followup_pdf$y))
 
   #################
   # sample params #
   #################
 
-  if (is.na(utility)) {
-    # QoL detriment: triangle(0.11, 0.21, 0.31)
-    utility <- rtriangle(n = nsim, a = 0.69, b = 0.89)  #1-QALY loss i.e. relative to non-TB (<1)
-  }else{
-    utility <- rep(utility, time = nsim)}
-
-  ## don't include generic tests costs
+  # exclude generic test costs
   costDistns$PET$params["mean"] <- 0
   costDistns$MRI$params["mean"] <- 0
   costDistns$CT$params["mean"]  <- 0
 
   cost <- health <- list()
 
-  for (i in 1:nsim) {
+  if ("PatientWgt" %in% names(data)) {
+    weight <- mean(data$PatientWgt, na.rm = TRUE)
+  }else{
+    weight <- 67.98}  #kg
 
-    # respiratory medicine, multi-professional (National tariff)
-    cost$visit1 <- rgamma(n = 1, shape = 53.3, scale = 4.52)
-    cost$visit2 <- rgamma(n = 1, shape = 18.78, scale = 7.62)
+  treatment.days <- 60
+  cost$twomonthTx <- treatment.days * (total_drug_cost(drug$rifamicin) +
+                                         total_drug_cost(drug$isoniazid) +
+                                         total_drug_cost(drug$pyrazinamid) +
+                                         total_drug_cost(drug$ethambutol, weight))
+
+  for (i in 1:nsim) {
 
     rperformance <- treeSimR::sample_distributions(performance)
     rcosts <- treeSimR::sample_distributions(costDistns)
-    t.ruleout <- unlist(treeSimR::sample_distributions(time_res))
+    t.newtest <- unlist(treeSimR::sample_distributions(time_res))
+    rQALYloss <- treeSimR::sample_distributions(QALYloss)/365.25
+
+    rfollowup <- inverse_sample(followup_cdf)
+
+    # respiratory medicine, multi-professional (National tariff)
+    cost$visit1 <- rcosts$visit1
+    cost$visit2 <- rcosts$visit2
 
     totalcost <- calcPatientCostofTests(data, COSTS = rcosts)
 
-    if ("PatientWgt" %in% names(data)) {
-      weight <- mean(data$PatientWgt, na.rm = TRUE)
-    }else{
-      weight <- 67.98}  #kg
-
     whoCat4Treated <- !is.na(data$TBDrugStart.min) & data$DosanjhGrouped == "4"
 
-    treatment.days <- 60
-    twomonthTreatCost <- treatment.days * ((rifamicin.cost_qty*rifamicin.mg_day)/(rifamicin.pill_qty*rifamicin.mg_pill) +
-                                             (isoniazid.cost_qty*isoniazid.mg_day)/(isoniazid.pill_qty*isoniazid.mg_pill) +
-                                             (pyrazinamide.cost_qty*pyrazinamide.mg_day)/(pyrazinamide.pill_qty*pyrazinamide.mg_pill) +
-                                             (ethambutol.cost_qty*ethambutol.mg_day_kg*weight)/(ethambutol.pill_qty*ethambutol.mg_pill))
+    totalcost[whoCat4Treated] <- totalcost[whoCat4Treated] + cost$twomonthTx
 
-    totalcost[whoCat4Treated] <- totalcost[whoCat4Treated] + twomonthTreatCost
+    if (!is.na(name.newtest)) cost$newtest <- rcosts[[name.newtest]]
 
-    if (!is.na(name.ruleout)) cost$ruleout <- rcosts[[name.ruleout]]
+    health$newtest <- rQALYloss$TB * t.newtest
 
-    health$ruleout <- utility[i] * t.ruleout
+    health$followup <- rQALYloss$TB * rfollowup
 
-    if (FNdist) {
-      health$FN <- utility[i] * FNdens$x[sum(runif(1) > Fx)]
-    }else{
-      health$FN <- utility[i]*FNtime}
-
-    TB  <- rbinom(n = 1, size = N, prob = prevalence)
-    nTB <- N - TB
-
-    # where are these numbers from?
-    TBhighrisk <- rbinom(n = 1, size = TB, prob = 1 - pbeta(cutoff, 7, 3))  # clinical positive diagnosis #dont currently use this
-    TBlowrisk  <- TB - TBhighrisk
-
-    nTBhighrisk <- rbinom(n = 1, size = nTB, prob = 1 - pbeta(cutoff, 3, 7))
-    nTBlowrisk  <- nTB - nTBhighrisk
-
-    TBpos <- rbinom(n = 1, size = TBlowrisk, prob = rperformance$sens)
-    TBneg <- TBlowrisk - TBpos
-
-    nTBpos <- rbinom(n = 1, size = nTBlowrisk, prob = 1 - rperformance$spec)
-    nTBneg <- nTBlowrisk - nTBpos
-
-    # final subpopulation sizes
-    pop <- c(TBhighrisk, nTBhighrisk, TBpos, TBneg, nTBpos, nTBneg)
+    health$twomonthTx <- (rQALYloss$TB + rQALYloss$Hepatotoxicity + rQALYloss$Nausea) * rfollowup
 
     # current observed time and cost estimates
+    # balanced samples
     sboot.nonTB <- sample(which(data$DosanjhGrouped == 4), replace = TRUE)
     sboot.TB <- sample(which(data$DosanjhGrouped %in% c(1,2,3)), replace = TRUE)
 
@@ -157,18 +133,42 @@ dectree <- function(data,
     start.to.diag.nonTB <- median(data$start.to.diag[sboot.nonTB]) %>% unname()
     start.to.diag.TB <- median(data$start.to.diag[sboot.TB]) %>% unname()
 
-    health$std.TB <- utility[i]*start.to.diag.TB
-    health$std.nonTB <- utility[i]*start.to.diag.nonTB
+    health$std.TB <- rQALYloss$TB * start.to.diag.TB
+    health$std.nonTB <- rQALYloss$TB * start.to.diag.nonTB
+
+    ################
+    # indiv sample #
+    ################
+
+    TB  <- rbinom(n = 1, size = N, prob = prevalence)
+    nTB <- N - TB
+
+    TBpos <- rbinom(n = 1, size = TB, prob = rperformance$sens)
+    TBneg <- TB - TBpos
+
+    nTBpos <- rbinom(n = 1, size = nTB, prob = 1 - rperformance$spec)
+    nTBneg <- nTB - nTBpos
+
+    # final subpopulation sizes
+    pop_std <- c(TB, nTB)
+    pop <- c(TBpos, TBneg, nTBpos, nTBneg)
 
     ##########
     # totals #
     ##########
 
-    Ec.old <- (cost$visit1*N + cost$std.TB*TB + cost$std.nonTB*nTB)/N     #THIS IS A BIT OF A PROBLEM FOR VARYING PREVALENCE IN ORDER TO BE A COMPARISON FOR ALL...
-    Ee.old <- (health$std.TB*TB + health$std.nonTB*nTB)/N                 #SIMILARLY THE SAMPLED CURRENT TIMES AND COSTS
+    terminal_cost_std <- c(cost$visit1 + cost$std.TB,
+                           cost$visit1 + cost$std.nonTB)
 
+    terminal_health_std <- c(health$std.TB,
+                             health$std.nonTB)
 
-    ## expected values
+    Ec.old <- (pop_std %*% terminal_cost_std)/N
+    Ee.old <- (pop_std %*% terminal_health_std)/N
+
+    #THIS IS A BIT OF A PROBLEM FOR VARYING PREVALENCE IN ORDER TO BE A COMPARISON FOR ALL...
+    #SIMILARLY THE SAMPLED CURRENT TIMES AND COSTS
+
     if (!is.na(Ec.old) & !is.na(Ee.old)) {
 
       e <- rbind(e,
@@ -176,14 +176,17 @@ dectree <- function(data,
       c <- rbind(c,
                  c(Ec.old, (pop %*% terminal_cost(cost))/N))
     }
-
   }
 
   colnames(e) <- c('e0', 'e1')
   colnames(c) <- c('c0', 'c1')
 
-  return(list(e = e,
-              c = c))
+  res <- list(e = e,
+              c = c)
+
+  class(res) <- append(class(res), "dectree")
+
+  return(res)
 }
 
 
