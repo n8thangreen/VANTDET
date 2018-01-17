22d04ab840a21f0f367f5c8450070f109c8c1f66
Dr Nathan Green <nathan.green@imperial.ac.uk>
2018-01-17 13:24:43 +0000

    dont use single reference status-quo but scenario specific in ce planes

--- scripts/output_ruleout.R
+++ scripts/output_ruleout.R
@@ -1,5 +1,6 @@
 #
-# VANTDET: output table of key cost-effectiveness statistics
+# VANTDET:
+# output table of key cost-effectiveness statistics for rule-out test
 # output_ruleout.R
 # N Green
 #
@@ -77,21 +78,34 @@ dat[['proteomic_flowassay']] <- dectree(
 #   terminal_health = terminal_health_ruleout)
 
 
-e_df <- do.call(cbind, purrr::map(dat, 'e'))[ ,c(1,2,4,6)]
-c_df <- do.call(cbind, purrr::map(dat, 'c'))[ ,c(1,2,4,6)]
+e_df <- do.call(cbind, purrr::map(dat, 'e'))
+c_df <- do.call(cbind, purrr::map(dat, 'c'))
 
-res_bcea <- bcea(e = e_df,
-                 c = -c_df)
+## use a common status-quo sample
+# e_df <- e_df[ ,c(1,2,4,6)]
+# e_df <- e_df[ ,c(1,2,4,6)]
+# res_bcea <- bcea(e = e_df,
+#                  c = -c_df,
+#                  interventions = c("status-quo", names(dat)))
+
+evens <- seq(from = 2, to = 2*length(dat), 2)
+odds <- evens - 1
+QALYgain <- as.matrix(data.frame(0, e_df[ ,odds] - e_df[ ,evens]))
+cost_incur <- as.matrix(data.frame(0, c_df[ ,evens] - c_df[ ,odds]))
+
+res_bcea <- bcea(e = -QALYgain,
+                 c = -cost_incur,
+                 interventions = c("status-quo", names(dat)))
 
 contour2(res_bcea, graph = "ggplot2")
-my_contour2(res_bcea, graph = "ggplot2", CONTOUR_PC = '50%')
+my_contour2(res_bcea, graph = "ggplot2", CONTOUR_PC = '5%') + coord_cartesian(xlim = c(-0.002, 0.002)) + theme(legend.position = "none")
 my_contour2_facet(dat)
 
 cost_effectiveness_table(dat)
-cost_effectiveness_table(res_bcea)
+result_tab <- cost_effectiveness_table(res_bcea)
 
 
 write.csv(x = result_tab,
-          file = "../../../output_data/ICERtable_ruleout.csv")
+          file = "output/ICERtable_ruleout.csv")
 
 
